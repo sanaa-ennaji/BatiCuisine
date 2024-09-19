dafdb303db3f9c9c815bf1c5a9ccea4c1950058c
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-09-19 12:49:17 +0100

    made database class fo cennection

--- src/main/java/ma/Bati/sc/Main.java
+++ src/main/java/ma/Bati/sc/Main.java
@@ -1,7 +1,19 @@
 package main.java.ma.Bati.sc;
 
+import main.java.ma.Bati.sc.utils.DatabaseConnection;
+
+import java.sql.Connection;
+import  java.sql.SQLException;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("setnence croisee!");
+        DatabaseConnection dbConnection = DatabaseConnection.getInstance();
+        try(Connection connection = dbConnection.getConnection())
+        {
+            System.out.println("setnence croisee!");
+        }catch(SQLException e){
+            e.printStackTrace();
+            System.out.println("failed to connect with database");
+        }
+
     }
 }
--- src/main/java/ma/Bati/sc/utils/database.java
+++
@@ -1,4 +0,0 @@
-package main.java.ma.Bati.sc.utils;
-
-public class database {
-}
