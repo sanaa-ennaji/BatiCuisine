328295aca970d94380aa0d6a7d5b30a77e417b5a
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-09-21 17:56:26 +0100

    add invalid expectations

---
+++ src/main/java/ma/Bati/sc/exception/InvalidDataException.java
@@ -0,0 +1,7 @@
+package main.java.ma.Bati.sc.exception;
+
+public class InvalidDataException extends RuntimeException {
+    public InvalidDataException(String message) {
+        super(message);
+    }
+}
