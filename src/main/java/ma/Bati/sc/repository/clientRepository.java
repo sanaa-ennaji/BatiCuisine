5aabe1212216b9223f1e839cc6301ae988c872f2
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-09-21 16:46:42 +0100

    add client implementation repository

--- src/main/java/ma/Bati/sc/repository/clientRepository.java
+++ src/main/java/ma/Bati/sc/repository/clientRepository.java
@@ -1,8 +1,44 @@
 package main.java.ma.Bati.sc.repository;
 
-public class clientRepository {
+import main.java.ma.Bati.sc.config.DatabaseConnection;
+import main.java.ma.Bati.sc.model.Client;
+import main.java.ma.Bati.sc.repository.Interfaces.IClientRepository;
 
+import java.sql.Connection;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class clientRepository implements IClientRepository {
+  private final Connection connection ;
     public  clientRepository(){
+   this.connection = DatabaseConnection.getInstance().getConnection();
+
+    }
+
+    @Override
+    public Client save(Client client) {
+        return null;
+    }
+
+    @Override
+    public List<Client> getAll() {
+        return List.of();
+    }
+
+    @Override
+    public Optional<Client> getById(UUID id) {
+        return Optional.empty();
+    }
+
+
+    @Override
+    public void update(Client client) {
+
+    }
+
+    @Override
+    public void delete(UUID id) {
 
     }
 }
