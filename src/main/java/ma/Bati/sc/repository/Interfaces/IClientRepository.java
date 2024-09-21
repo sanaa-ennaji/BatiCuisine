a4b1bed5a29c45c0a4fbbba9dc1759b88741d688
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-09-21 15:29:34 +0100

    add a client interface

--- src/main/java/ma/Bati/sc/repository/Interfaces/IClientRepository.java
+++ src/main/java/ma/Bati/sc/repository/Interfaces/IClientRepository.java
@@ -1,6 +1,16 @@
 package main.java.ma.Bati.sc.repository.Interfaces;
 
-public interface IClientRepository {
+import main.java.ma.Bati.sc.model.Client;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface IClientRepository {
+    List <Client>  getAll ();
+    Optional <Client> getById(UUID id);
+    Client save (Client client);
+    void update(Client client);
+    void delete (UUID id);
 
 }
