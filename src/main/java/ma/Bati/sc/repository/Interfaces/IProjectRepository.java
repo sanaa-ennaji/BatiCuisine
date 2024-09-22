0438e3fee2951df04c5f69057eb729002cc86708
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-09-22 17:29:03 +0100

    add project repo interface

--- src/main/java/ma/Bati/sc/repository/Interfaces/IProjectRepository.java
+++ src/main/java/ma/Bati/sc/repository/Interfaces/IProjectRepository.java
@@ -1,4 +1,18 @@
 package main.java.ma.Bati.sc.repository.Interfaces;
 
-public class IProjectRepository {
+import main.java.ma.Bati.sc.model.Project;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface IProjectRepository {
+
+   Project save (Project project);
+   List<Project> getAll();
+   Optional<Project> getById(UUID id);
+
+
+
+
 }
