d2fab73e88b27e8d9a551e6d9237b1442b0a5c2d
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-09-22 02:10:36 +0100

    updating client service

--- src/main/java/ma/Bati/sc/service/clientService.java
+++ src/main/java/ma/Bati/sc/service/clientService.java
@@ -1,4 +1,41 @@
 package main.java.ma.Bati.sc.service;
 
-public class clientService {
+import main.java.ma.Bati.sc.model.Client;
+import main.java.ma.Bati.sc.repository.Interfaces.IClientRepository;
+import main.java.ma.Bati.sc.service.IService.IClientService;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class clientService  implements IClientService{
+  private final IClientRepository clientRepository;
+    public clientService (IClientRepository clientRepository){
+
+        this.clientRepository = clientRepository;
+    }
+
+
+    @Override
+    public Client create(Client client) {
+        if (client.getName() == null || client.getName().trim().isEmpty()){
+            throw new RuntimeException();
+        }
+        return clientRepository.save(client);
+    }
+
+    @Override
+    public List<Client> getAll() {
+        return List.of();
+    }
+
+    @Override
+    public Optional<Client> findById(UUID id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void delete(UUID id) {
+
+    }
 }
