8791e9379d4940af54903dd242201c8af8640c85
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-09-24 10:03:31 +0100

    modefy client UI and menu

--- src/main/java/ma/Bati/sc/UI/ClientUI.java
+++ src/main/java/ma/Bati/sc/UI/ClientUI.java
@@ -1,4 +1,41 @@
 package main.java.ma.Bati.sc.UI;
 
+import main.java.ma.Bati.sc.model.Client;
+import main.java.ma.Bati.sc.service.IService.IClientService;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class ClientUI {
+    private final IClientService clientService ;
+    private  final Scanner scanner = new Scanner(System .in);
+
+
+
+    public ClientUI(IClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    public void createClient(){
+        System.out.print("enter the name: ");
+        String name = scanner.nextLine();
+        System.out.println("enter the address: ");
+        String address = scanner.nextLine();
+        System.out.println("enter the phone number: ");
+        String phone = scanner.nextLine();
+        System.out.println("is professional (Y/n)");
+        boolean isProfessional = Boolean.parseBoolean(scanner.nextLine());
+
+        Client client = new Client(
+                name,
+                address,
+                phone,
+                isProfessional
+
+        );
+        clientService.create(client);
+        System.out.println("client createation done");
+
+    }
+
 }
