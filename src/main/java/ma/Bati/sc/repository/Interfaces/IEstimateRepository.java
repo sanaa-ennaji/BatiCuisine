06c3a28df28602b78b06f33e5c6486abb44dc5e6
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-09-24 20:36:30 +0100

    add estimate constructor

--- src/main/java/ma/Bati/sc/repository/Interfaces/IEstimateRepository.java
+++ src/main/java/ma/Bati/sc/repository/Interfaces/IEstimateRepository.java
@@ -1,5 +1,10 @@
 package main.java.ma.Bati.sc.repository.Interfaces;
 
+import main.java.ma.Bati.sc.model.Estimate;
+
+import java.util.Optional;
+import java.util.UUID;
+
 public interface IEstimateRepository {
     void save(Estimate estimate);
     Optional<Estimate> findById(UUID id);
--- src/main/java/ma/Bati/sc/repository/estimateRepository.java
+++ src/main/java/ma/Bati/sc/repository/estimateRepository.java
@@ -1,4 +1,28 @@
 package main.java.ma.Bati.sc.repository;
 
-public class estimateRepository {
+import main.java.ma.Bati.sc.config.DatabaseConnection;
+import main.java.ma.Bati.sc.model.Estimate;
+import main.java.ma.Bati.sc.repository.Interfaces.IEstimateRepository;
+
+import java.sql.Connection;
+import java.util.Optional;
+import java.util.UUID;
+
+public class estimateRepository implements IEstimateRepository {
+
+    private final Connection connection;
+
+    public estimateRepository() {
+        this.connection = DatabaseConnection.getInstance().getConnection();
+    }
+
+    @Override
+    public void save(Estimate estimate) {
+
+    }
+
+    @Override
+    public Optional<Estimate> findById(UUID id) {
+        return Optional.empty();
+    }
 }
